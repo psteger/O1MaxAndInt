b18ffa9f1596b97f72559eae8352e4adc792d9a7
Patrick Steger <brain007@gmail.com>
2017-02-26 18:20:37 -0500

    Add files via upload

---
+++ O1MaxAndInt/IntMaxStack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O1MaxAndInt
+{
+    public class IntMaxStack
+    {
+        public class Node
+        {
+            public int num = 0;
+            public int max = 0;
+        }
+
+        LinkedList<Node> list = new LinkedList<Node>();
+
+        public void Push(int i)
+        {
+            // Gets a bit unreadable in there checking for a null last value.
+            Node n = new Node() { num = i, max = (list.Last?.Value?.max ?? int.MinValue) > i ? list.Last.Value.max : i };
+            list.AddLast(n);
+        }
+
+        public void Remove()
+        {
+            if (list.Count > 0)
+                list.RemoveLast();
+        }
+
+        public int GetMax()
+        {
+            if (list.Count > 0)
+                return list.Last.Value.max;
+            return -1;  // Unknown spec from assignment. Assuming -1
+        }
+    }
+}
---
+++ O1MaxAndIntTests/IntStackTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using O1MaxAndInt;
+
+namespace O1MaxAndIntTests
+{
+    [TestClass]
+    public class IntStackTests
+    {
+        [TestMethod]
+        public void NoValuesGetMax()
+        {
+            IntMaxStack i = new IntMaxStack();
+            int n = i.GetMax();
+            Assert.AreEqual(n, -1);
+        }
+
+        [TestMethod]
+        public void OneValueGetMax()
+        {
+            IntMaxStack i = new IntMaxStack();
+            i.Push(5);
+            int n = i.GetMax();
+            Assert.AreEqual(n, 5);
+        }
+
+        [TestMethod]
+        public void TwoValuesGetMax()
+        {
+            IntMaxStack i = new IntMaxStack();
+            i.Push(5);
+            i.Push(4);
+            int n = i.GetMax();
+            Assert.AreEqual(n, 5);
+        }
+
+        [TestMethod]
+        public void MultValueRemoveGetMax()
+        {
+            IntMaxStack i = new IntMaxStack();
+            i.Push(5);
+            i.Push(4);
+            i.Push(9);
+            i.Push(12);
+            i.Remove();
+            int n = i.GetMax();
+            Assert.AreEqual(n, 9);
+        }
+
+        [TestMethod]
+        public void MoreRemoveThanPushGetMax()
+        {
+            IntMaxStack i = new IntMaxStack();
+            i.Push(5);
+            i.Push(4);
+            i.Push(9);
+            i.Push(12);
+            i.Remove();
+            int n = i.GetMax();
+            Assert.AreEqual(n, 9);
+            i.Remove();
+            n = i.GetMax();
+            Assert.AreEqual(n, 5);
+            i.Remove();
+            n = i.GetMax();
+            Assert.AreEqual(n, 5);
+            i.Remove();
+            n = i.GetMax();
+            Assert.AreEqual(n, -1);
+            i.Remove();
+            n = i.GetMax();
+            Assert.AreEqual(n, -1);
+        }
+    }
+}
